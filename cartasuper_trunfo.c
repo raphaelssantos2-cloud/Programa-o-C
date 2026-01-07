23d0a3c564e6922b4d0c778ff8357199d54e44c2
Raphael Santos <raphael.ssantos2@gmail.com>
2026-01-07 10:44:03 -0300

    Refactor city data representation in cartasuper_trunfo.c

--- cartasuper_trunfo.c
+++ cartasuper_trunfo.c
@@ -1,36 +1,31 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das cartas
-// Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
-
 int main() {
-  char Estado = A;
-  char Codigo = A01;
-  char Cidade = Sao_Paulo;
-  int Populucao = 12325000;
-  float Area = 1521,11 km²;
-  float PIB = 699,28 Bi_reais;
-  int Pontos_turisticos = 50;
+char Estado[] = "São Paulo";
+    char Codigo[] = "A01";
+    char Cidade[] = "São Paulo";
+    int Populacao = 12325000;
+    float Area = 1521.11;
+    float PIB = 699.28;
+    int Pontos_turisticos = 50;
+
+    char Estado2[] = "Rio de janeiro";
+    char Codigo2[] = "B02";
+    char Cidade2[] = "Rio de janeiro";
+    int Populacao2 = 6748000;
+    float area2 = 1200.25;
+    float PIB2 = 300.50;
+    int Ponto_turisticos2 = 30;
+
+    printf("Estado: %s\n", Estado);
+    printf("Codigo: %s\n", Codigo);
+    printf("Cidade: %s\n", Cidade);
+    printf("População: %d\n", Populacao);
+    printf("Área: %.2f\n", Area);
+    printf("PIB: %.2f\n", PIB);
+    printf("Pontos turísticos: %d\n", Pontos_turisticos);
+
   
-Estado: (A a H) - char
-Código: (Ex: A01) - char[] (string)
-Cidade: (Nome) - char[] (string)
-População: (Número de habitantes) - int
-Área: (em km²) - float
-PIB: (em bilhões) - float
-Pontos Turísticos: (Quantidade) - int
-
-  Estado: (A a H) - char
-Código: (Ex: A01) - char[] (string)
-Cidade: (Nome) - char[] (string)
-População: (Número de habitantes) - int
-Área: (em km²) - float
-PIB: (em bilhões) - float
-Pontos Turísticos: (Quantidade) - int
-  // Área para entrada de dados
-
-  // Área para exibição dos dados da cidade
 
 return 0;
 } 
