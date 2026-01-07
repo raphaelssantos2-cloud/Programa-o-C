673e9656994348180a0b0f9a5c584a07ef435af0
Raphael Santos <raphael.ssantos2@gmail.com>
2026-01-07 09:50:29 -0300

    Create cartasuper_trunfo.c for city card details
    Initial setup for Super Trunfo card game with city properties.

--- cartasuper_trunfo.c
+++ cartasuper_trunfo.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
 
+// Desafio Super Trunfo - Países
+// Tema 1 - Cadastro das cartas
+// Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
+
+int main() {
+  // Área para definição das variáveis para armazenar as propriedades das cidades
+Estado: (A a H) - char
+Código: (Ex: A01) - char[] (string)
+Cidade: (Nome) - char[] (string)
+População: (Número de habitantes) - int
+Área: (em km²) - float
+PIB: (em bilhões) - float
+Pontos Turísticos: (Quantidade) - int
+
+  Estado: (A a H) - char
+Código: (Ex: A01) - char[] (string)
+Cidade: (Nome) - char[] (string)
+População: (Número de habitantes) - int
+Área: (em km²) - float
+PIB: (em bilhões) - float
+Pontos Turísticos: (Quantidade) - int
+  // Área para entrada de dados
+
+  // Área para exibição dos dados da cidade
+
+return 0;
+} 
